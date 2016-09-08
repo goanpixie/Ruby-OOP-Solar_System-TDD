0901a3aefc9d1a52d213e5e1159a9caa4052c921
Priyanka <priyanka3007@gmail.com>
2016-09-08 17:46:19 -0400

    SuperClass

---
+++ solar_system.rb
@@ -0,0 +1,19 @@
+class Solar_System
+  attr_accessor :name,:planets
+
+    def initialize
+      @name = name
+      @planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune" ]
+    end
+
+    def count_planets
+      @planets.count
+    end
+
+    def SuperNova
+      @planets.clear
+      end
+end
+
+
+
