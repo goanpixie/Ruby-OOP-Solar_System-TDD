be3bf14f4b3c9d4c1703e05c5ae775b5cf114528
Priyanka <priyanka3007@gmail.com>
2016-09-08 18:21:05 -0400

    All methods completed

--- planet.rb
+++ planet.rb
@@ -1,9 +1,11 @@
 require_relative "Solar_System"
 
 class Planet < Solar_System
-  attr_accessor :description, :population
+  attr_accessor :description, :population, :planets
 
-	def add_planets=(planet)
+	def add_planets(planet)
 		@planets.push(planet)
 	end
 end
+
+puts Planet.new.add_planets("Quizer")
